64bb0dc3d64f3409db7168972870e5587d714dbe
benjaLopez19 <82004309+benjaLopez19@users.noreply.github.com>
2021-05-17 19:29:13 -0400

    Update servicio-nota.service.ts

--- src/app/servicio-nota.service.ts
+++ src/app/servicio-nota.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http"
+import {Observable} from "rxjs";
+import {Nota} from "./nota";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServicioNotaService {
 
-  constructor() { }
+  url="";
+  constructor(private http:HttpClient) { }
+
+  consultarNotas():Observable<any>{
+    return this.http.get(`${this.url}inicio.php`);
+  }
+
+  guardarDatos(lista:Array<Nota>):Observable<any>{
+    return this.http.post(`${this.url}guardar.php`, JSON.stringify(lista));
+  }
 }
